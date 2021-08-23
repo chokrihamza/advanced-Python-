261bb6a3fef9ee8735e10d35281272f4fb8348a7
chokrihamza <chokrihamza1994@gmail.com>
2021-08-23 16:35:32 +0200

    create class decorator like flask

--- decorate_a_class_in_python.py
+++ decorate_a_class_in_python.py
@@ -1,26 +1,37 @@
 from datetime import datetime
 
-class DateDecorator(object):
+class Add_DateDecorator(object):
     # instance method decorator
     def instantMethodDecorator(self, func):
-        def printDate(*args, **kwargs):
-            print("Instance method decorator at time : \n", datetime.today())
-            return func(*args, **kwargs)
+        def printDate(*args):
+            print('*'*40)
+            print("Instance method decorator at time :", datetime.today())
+            print('The result of sum is: ',func(*args))
+            print('*' * 40)
         return printDate
 
     # class method decorator
     @classmethod
     def classMethodDecorator(self, func):
-        def printDate(*args, **kwargs):
+        def printDate(*args):
+            print('*' * 40)
             print("Class method decorator at time : \n", datetime.today())
-            return func(*args, **kwargs)
+            print('The result of sum is: ',func(*args))
+            print('*' * 40)
         return printDate
 
 
 # decorator: instance method
-a = DateDecorator()
+a = Add_DateDecorator()
 @a.instantMethodDecorator
-def add(a, b):
-    return a+b
+def add(*a):
+    return sum(a)
 
 add(5,3)
+
+# lets use the class method
+@Add_DateDecorator.classMethodDecorator
+def add(*a):
+    return sum(a)
+
+add(5,10)
